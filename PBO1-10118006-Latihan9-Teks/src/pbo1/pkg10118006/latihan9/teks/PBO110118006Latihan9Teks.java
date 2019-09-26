b9ace885a131a5935465c8eb491307b8332a9749
MrBradabie <55830472+MrBradabie@users.noreply.github.com>
2019-09-26 16:50:38 +0700

    Add files via upload

---
+++ PBO1-10118006-Latihan9-Teks/src/pbo1/pkg10118006/latihan9/teks/PBO110118006Latihan9Teks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118006.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * NAMA  : Alfarizi Rizki Pane
+ * KELAS : PBO1
+ * NIM   : 10118006
+ * Deskripsi Program : Program ini berisi  program untuk teks
+ */
+public class PBO110118006Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab ='\t';
+        char newline ='\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
